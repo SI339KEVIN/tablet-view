84b472628b70b897b677c6fc0f73d6ffe818e16b
SI339KEVIN <144163686+SI339KEVIN@users.noreply.github.com>
2023-11-05 17:12:50 -0500

    Correct Files

---
+++ js/script.js
@@ -0,0 +1,16 @@
+function toggleDarkMode() {
+    var body = document.body;
+    body.classList.toggle('dark-mode');
+
+    var card = document.getElementsByClassName('card');
+    for (var i = 0; i < card.length; i++) {
+        card[i].classList.toggle('dark-mode');
+    }
+
+    var card = document.getElementsByClassName('card-travel');
+    for (var i = 0; i < card.length; i++) {
+        card[i].classList.toggle('dark-mode');
+    }
+
+    console.log("Dark mode toggled");
+  }
